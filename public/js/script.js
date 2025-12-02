37b9b51fd31478d690d8b50e0e8e6a76eb684421
discotechnology <drosenfeld@csumb.edu>
2025-12-02 06:08:22 -0800

    Added modal to display author info

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,24 @@ for (link of authorLinks) {
 }
 
 async function getAuthorInfo() {
+    var myModal = new bootstrap.Modal(document.getElementById('authorModal'));
+    myModal.show();
+
     let url = `/api/author/${this.id}`;
     let response = await fetch(url);
     let data = await response.json();
+
+    let dob = data[0].dob.substring(0,10);
+    let dod = data[0].dod.substring(0,10);
     
     let authorInfo = document.querySelector('#authorInfo');
     authorInfo.innerHTML = `<h2> ${data[0].firstName} ${data[0].lastName} </h2>
                             <img src='${data[0].portrait}' alt='Portrait of ${data[0].firstName} ${data[0].lastName}' width='200'><br>
+                            <strong>Born:</strong> ${dob} <br>
+                            <strong>Died:</strong> ${dod} <br>
+                            <strong>Country:</strong> ${data[0].country} <br>
+                            <strong>Profession:</strong> ${data[0].profession} <br>
+                            <strong>Sex:</strong> ${data[0].sex} <br>
                             <strong>Bio:</strong> ${data[0].biography} <br>
-                            <strong>Born:</strong> ${data[0].dob} <br>
-                            <strong>Died:</strong> ${data[0].dod} <br>
                             `;
 }
